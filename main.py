3984100b70bd9ce3701d180c26f16688007e1404
rainbow1223 <devstar0611@gmail.com>
2023-06-29 05:51:30 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from pyppeteer import launch
+import requests
 from bs4 import BeautifulSoup
 import time
 import smtplib, ssl
@@ -32,43 +33,49 @@ Subject: {subject}
         server.login(server_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
-async def scraper():
+def scraper():
+    try:
 
-    browser = await launch(ignoreHTTPSErrors=True)
-    page = await browser.newPage()
-    page.setDefaultNavigationTimeout(60000); # Increases the timeout limit to 60 seconds
+        # browser = launch(ignoreHTTPSErrors=True)
+        # page = browser.newPage()
+        # page.setDefaultNavigationTimeout(60000); # Increases the timeout limit to 60 seconds
 
 
-    await page.goto('https://betdiary.io/tipster/bets/4915/hamexrodregan/')
-    await page.waitForSelector('div[id^="kupong"]', timeout=5000)
-    html = await page.content()
-    soup = BeautifulSoup(html, 'html.parser')
-    element = soup.select_one('div[id^="kupong"]:first-child')
-    id = element['id'].split('-')[-1]
-    global history, isStart
-    if 'open' in element['data-betfilter'] and id not in history:
-        title = list(element.select_one('.card-header > div > .col-lg-6:first-child').children)[-1].text
-        content = "Title: " + title + '\n'
-        content += list(element.select_one('.card-header > div > .col-lg-6:last-child').children)[3].text.split(', ')[-1] + '\n'
-        for ele in element.select('.card-block > div > div.o'):
-            content += ele.text + '\n'
-        print(content)
-        history.append(id)
-        if (not isStart):
-            isStart = 1
+        # page.goto('https://betdiary.io/tipster/bets/4915/hamexrodregan/')
+        # page.waitForSelector('div[id^="kupong"]', timeout=5000)
+        # html = page.content()
+        response = requests.get("https://betdiary.io/tipster/bets/4915/hamexrodregan/")
+        time.sleep(1)
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        element = soup.select_one('div[id^="kupong"]:first-child')
+
+        id = element['id'].split('-')[-1]
+        global history, isStart
+        if 'open' in element['data-betfilter'] and id not in history:
+            title = list(element.select_one('.card-header > div > .col-lg-6:first-child').children)[-1].text
+            content = "Title: " + title + '\n'
+            content += list(element.select_one('.card-header > div > .col-lg-6:last-child').children)[3].text.split(', ')[-1] + '\n'
+            for ele in element.select('.card-block > div > div.o'):
+                content += ele.text + '\n'
+            print(content)
+            if (not isStart):
+                isStart = 1
+            else:
+                send_mail("A new post by hamexrodregan", content)
+            history.append(id)
         else:
-            send_mail("A new post by hamexrodregan", content)
-    else:
-        print('not open or already exist')
+            print('not open or already exist')
 
-    await browser.close()
+    except Exception as err:
+        print(err)
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     while not loop.is_closed():
-        loop.run_until_complete(scraper())
+        scraper()
         time.sleep(1)
 
     
